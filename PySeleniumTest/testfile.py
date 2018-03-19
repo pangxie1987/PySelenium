ce04ae94295348e65f7cfe628497faf35e51c73c
pangxie1987 <lpb.waln@outlook.com>
2018-03-19 09:15:46 +0800

    'testfile-modify'

--- PySeleniumTest/testfile.py
+++ PySeleniumTest/testfile.py
@@ -1,8 +1,14 @@
 #-*- coding:utf-8 -*-
 import time
+import sys
+sys.path.append('C:\Python\Python34\Lib\site-packages')
+sys.path.append('C:\WorkDay\Code\Python\PySeleniumTest')
 from datetime import datetime
 from selenium import webdriver
-from driverset.webDriverset import driver
+#from driverset.webDriverset import driver
+import readtext
+import Email
+
 dt=datetime.now()
 print(dt.strftime('%H:%M:%S'))
-
+print('hello jenkins')
