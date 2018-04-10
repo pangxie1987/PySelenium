b1010cb3cf3d88b49bf61e3720b1cab71438a98c
unknown <pingbao.liu@kingstar.com>
2018-04-10 16:48:10 +0800

    'repair'

--- PySeleniumTest/testfile.py
+++ PySeleniumTest/testfile.py
@@ -6,8 +6,8 @@ sys.path.append('C:\WorkDay\Code\Python\PySeleniumTest')
 from datetime import datetime
 from selenium import webdriver
 #from driverset.webDriverset import driver
-import readtext
-import Email
+# #import readtext
+#import Email
 
 dt=datetime.now()
 print(dt.strftime('%H:%M:%S'))
